2ecdb4ae779af71a1028841e298bdde36fdb86f3
Sabitha NH <sabitha.nh@gmail.com>
2016-09-28 11:18:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article_one', function(req, res){
-    res.send('Article one is requested amd will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
     });
 app.get('/article_two', function(req, res){
     res.send('Article two is requested amd will be served here');
