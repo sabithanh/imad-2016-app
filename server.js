d17ac828c000d0278e203548f0125032cd562212
Sabitha NH <sabitha.nh@gmail.com>
2016-09-29 12:26:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 var articleOne = {
     title: "Article one | Sabitha",
     heading: "Article one",
-    date: 28/09/2016,
+    date: '28/09/2016',
     content: `<p>
                     This is the content of the article one. This is the content of the article one. This is the content of the article one. This is the content of the article one. This is the content of the article one. This is the content of the article one. This is the content of the article one. 
                 </p>
