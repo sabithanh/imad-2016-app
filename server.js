b5dc8f3f66bb03bf4b0901f76414549ebd3f2e5a
Sabitha NH <sabitha.nh@gmail.com>
2016-09-29 13:28:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName', function(req,res){
     var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articeName]));
+    res.send(createTemplate(articles[articleName]));
 });
 /*app.get('/article_one', function(req, res){
     res.send(createTemplate(articles.articleOne));
