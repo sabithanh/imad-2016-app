cf23e9a808c5dfada39ba2f6b7b8411d42d52833
Sabitha NH <sabitha.nh@gmail.com>
2016-09-29 21:02:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//change the text of the image
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+
+//move the image upon click
+var element = document.getElementById('madi');
+madi.onclick = function(){
+    madi.style.marginLeft = '100px';
+};
