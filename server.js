08ff2a442e12685719b77a8bd45d30910252745a
Sabitha NH <sabitha.nh@gmail.com>
2016-10-10 19:16:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/images/profile.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'profile.jpg'));
+  res.sendFile(path.join(__dirname, 'ui/images', 'profile.jpg'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
