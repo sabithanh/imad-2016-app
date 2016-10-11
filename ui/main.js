049845edf560b055612669dc1b3c7b95f526b081
Sabitha NH <sabitha.nh@gmail.com>
2016-10-11 09:09:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,23 @@
 //Counter code
 var button=document.getElementById('counter');
-counter=0;
+
 button.onclick = function (){
   //make a request to teh counter endpoint
+  var request = new XMLHttpRequest();
   
   //capture the response and store it in a variable
-  
-  //render the variabel in the correct span
-  counter =counter+1;
-  var span = document.getElementById('count');
-  span.innerHTML=counter.toString();
+  request.onReadystateChange= function (){
+      if (request.readyState === XMLHttpRequest.DONE){
+          //take some action
+          if(request.status==200){
+              var counter=request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+      //not done yet
+  };
+  //make the request
+  request.open('GET', 'http://sabithanh.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
