e0a5daa055c0bd91e262d2ab4b8048063a586d15
Sabitha NH <sabitha.nh@gmail.com>
2016-10-11 09:53:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ button.onclick = function (){
   request.open('GET', 'http://sabithanh.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+//Submit name
+var nameInput=document.getelementByid('name');
+var name=nameInput.value;
+var submit = document.GetelementById('submit_btn');
+submit.onclick = funtcion () {
+  //make a request to the server and send the name
+  //capture the list of names and render it as a list
+  var names =['name1', 'name2', 'name3','name4'];
+  var list ='';
+  for (var i=0; i<names.length; i++){
+      list=list+ <li> + names[i] + </li>;
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
